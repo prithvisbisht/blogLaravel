bcd22d16868d21de3dc75d2ce9dfd305c4820ab1
Prithvi Raj singh Bisht <prithvisbisht@gmail.com>
2018-04-04 21:57:34 +1100

    basic routes added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/second', function () {
+//     //return view('welcome');
+//     return "<h1>This is Second page<h1>";
+// });
+// Route::get('/users/{id}/{name}',function($id,$name){
+//     return 'this is user '.$name.' with an id of '.$id;    
+// });
+
+Route::get('/','PagesController@index');
+Route::get('/about','PagesController@about');
+Route::get('/services','PagesController@services');
